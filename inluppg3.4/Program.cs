cadffbdecf031c9e6f4f716b3f159184e612119c
emsi06001 <emsi06001@E-PW0234N3>
2023-10-04 12:35:57 +0200

    Add project files.

---
+++ inluppg3.4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace inluppg3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många hela minuter är din låt?");
+            int minuter = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många resterande sekunder är din låt?");
+            int sekunder = int.Parse(Console.ReadLine());
+
+            int totalaSekunder = (minuter * 60) + sekunder;
+            if (totalaSekunder <= 4*60+20 && totalaSekunder >= 2 * 60 + 45)
+            {
+                Console.WriteLine("Din låt får spelas på stationen");
+            }
+            else
+            {
+                Console.WriteLine("Din låt får inte spelas på stationen");
+            }
+
+        }
+    }
+}
